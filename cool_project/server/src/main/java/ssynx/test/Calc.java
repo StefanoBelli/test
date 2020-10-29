2545c61e84f10d2cb626898b9418c499dfb6ecff
ssynx <sa.belli@hotmail.it>
2020-10-29 16:06:48 +0100

    sc mvn

---
+++ cool_project/server/src/main/java/ssynx/test/Calc.java
@@ -0,0 +1,8 @@
+package ssynx.test;
+
+public class Calc {
+	public static int sum(int x, int y) {
+		return x + y;
+	}
+}
+
---
+++ cool_project/server/src/test/java/ssynx/test/CalcTest.java
@@ -0,0 +1,26 @@
+package ssynx.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalcTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testSum() 
+    {
+    	assertTrue(Calc.sum(1,2) == 3);
+    }
+}
